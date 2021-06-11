3c2aabb2bdfebda6e03605b4e8fcb9082ab406ed
AbdulNishar05 <80945971+AbdulNishar05@users.noreply.github.com>
2021-06-11 11:25:43 +0530

    Update details.php

--- phpAssessment/details.php
+++ phpAssessment/details.php
@@ -1,5 +1,74 @@
 <?php include 'Db.php' ?>
 <!DOCTYPE html>
+<?php
+session_start();
+if (!empty($_GET['action']))
+{
+    switch ($_GET['action'])
+    {
+        case 'Add':
+            if (!empty($_POST['quantity']))
+            {
+
+                $id = $_GET['Id'];
+                $query = "SELECT * FROM products WHERE Id=" . $id;
+                $result = mysqli_query($connection, $query);
+                while ($product = mysqli_fetch_array($result))
+                {
+                    $itemArray = [$product['Sku'] => ['Name' => $product['Name'], 'Sku' => $product['Sku'], 'quantity' => $_POST['quantity'], 'Price' => $product['Price'], 'image' => $product['image']]];
+                    if (isset($_SESSION['cart_item']) && !empty($_SESSION['cart_item']))
+                    
+                    {
+                        if (in_array($product['Sku'], array_keys($_SESSION['cart_item'])))
+                        {
+                            foreach ($_SESSION['cart_item'] as $key => $value)
+                            {
+                                if ($product['Sku'] == $key)
+                                {
+                                    if (empty($_SESSION['cart_item'][$key]["quantity"]))
+                                    {
+                                        $_SESSION['cart_item'][$key]['quantity'] = 0;
+                                    }
+                                    $_SESSION['cart_item'][$key]['quantity'] += $_POST['quantity'];
+                                }
+                            }
+                        }
+                        else
+                        {
+                            $_SESSION['cart_item'] += $itemArray;
+                        }
+                    }
+                    else
+                    {
+                        $_SESSION['cart_item'] = $itemArray;
+                    }
+                }
+            }
+        break;
+        case 'remove':
+            if (!empty($_SESSION['cart_item']))
+            {
+                foreach ($_SESSION['cart_item'] as $key => $value)
+                {
+                    if ($_GET['Sku'] == $key)
+                    {
+                        unset($_SESSION['cart_item'][$key]);
+                    }
+                    if (empty($_SESSION['cart_item']))
+                    {
+                        unset($_SESSION['cart_item']);
+                    }
+                }
+            }
+        break;
+        case 'empty':
+            unset($_SESSION['cart_item']);
+        break;
+    }
+}
+?>
+
+
 <html lang="en">
    <head>
       <meta charset="UTF-8">
@@ -14,6 +83,68 @@
       <div class="container">
       <h1 align="center">PDP</h1>
       <div class="row">
+      <div>
+        <h3> Cart </h3>
+        </div>
+        <table class="table">
+            <tbody>
+            <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Sku</th>
+                <th class="text-right">Quantity</th>
+                <th class="text-right">Item Price</th>
+                <th class="text-right">Price</th>
+                <th class="text-center">Remove</th>
+            </tr>
+            <?php
+            $total_quantity=0;
+            $total_Price =0;
+if (isset($_SESSION['cart_item']) && !empty($_SESSION['cart_item']))
+{
+    foreach ($_SESSION['cart_item'] as $item)
+    {
+        $item_Price = $item['quantity'] * $item['Price'];
+?>
+                    <tr>
+                        <td class="text-left">
+                            <img src="<?=$item['image'] ?>" alt="<?=$item['Name'] ?>" class="img-fluid" width="100">
+                            <?=$item['Name'] ?>
+                        </td>
+                        <td class="text-left"><?=$item['Sku'] ?></td>
+                        <td class="text-right"><?=$item['quantity'] ?></td>
+                        <td class="text-right">₹<?=number_format($item['Price'], 2) ?></td>
+                        <td class="text-right">₹<?=number_format($item_Price, 2) ?></td>
+                        <td class="text-center">
+                            <a href="details.php?action=remove&Sku=<?=$item['Sku']; ?>" class="btn btn-danger">X</a>
+                        </td>
+                    </tr>
+
+                    <?php
+         
+        $total_quantity += $item["quantity"];
+        $total_Price += ($item["Price"] * $item["quantity"]);
+    }
+}
+
+if (isset($_SESSION['cart_item']) && !empty($_SESSION['cart_item']))
+{
+?>
+                <tr>
+                    <td colspan="2" align="right">Total:</td>
+                    <td align="right"><strong><?=$total_quantity
+?></strong></td>
+                    <td></td>
+                    <td align="right"><strong>₹<?=number_format($total_Price, 2); ?></strong></td>
+                    <td></td>
+                </tr>
+
+            <?php
+}
+
+?>
+            </tbody>
+        </table>
+    </div>
       <h3>Details</h3>
       <?php
 if (isset($_GET['Id']))
@@ -27,7 +158,7 @@ if (isset($_GET['Id']))
 ?>
       <table class="table table-bordered">
       <tr id="tr">
-         <td > <img src="<?php echo $row["image"]; ?>" width="300" height="300" ></td>
+         <td > <img src="<?php echo $row["image"]; ?>" ></td>
       <tr>
          <td>
             <p>Name:<strong><?php echo $row['Name']; ?></p>
@@ -44,6 +175,11 @@ if (isset($_GET['Id']))
          <td>
             <p>Price:<strong><?php echo $row['Price']; ?></p>
             </strong>
+            <form action="details.php?action=Add&Id=<?=$row['Id']; ?>" method="post">
+            <td> <input type="number" Name="quantity" value="1" min=1 max=1000 class="number"></td>
+
+            <td><input type="submit" value="Add to Cart" class="btn btn-success btn-sm"></td>
+            </form>
             <?php
     }
     else
@@ -56,5 +192,6 @@ else
     header("location:index.php");
 }
 ?>
+
    </body>
 </html>
