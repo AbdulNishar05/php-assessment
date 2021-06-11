716662b97178f0d8109d4b0480e7ba7b3c04d767
AbdulNishar05 <80945971+AbdulNishar05@users.noreply.github.com>
2021-06-11 11:27:20 +0530

    Update Db.php

--- phpAssessment/Db.php
+++ phpAssessment/Db.php
@@ -1,5 +1,18 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "Admin@123", "plp");
-if (!$connection) {
-    die("not connected" . connect_error());
+try {
+  if($connection = mysqli_connect("localhost", "root", "Admin@123", "plp"))
+  {
+    
+  }
+    
+else
+    {
+        throw new Exception('Unable to connect');
+    }
+}    
+catch(Exception $e)
+{
+    echo $e->getMessage();
 }
+
+?>
